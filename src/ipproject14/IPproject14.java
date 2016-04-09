89369a429ee1af858175a66fcbfef5136bbd15eb
tanisha shrotriya <tanisha shrotriya>
2016-04-09 20:47:19 +0530

    final commit

---
+++ src/ipproject14/IPproject14.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipproject14;
+
+/**
+ *
+ * @author shrav
+ */
+public class IPproject14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Welcome().setVisible(true);
+            }
+        });
+    }
+}
